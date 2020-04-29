eba9296ca982244360899e7dda7f30c45996185a
Ryan Mathis <rmathis@unicon.net>
2020-04-29 07:38:46 -0700

    Refactored Error component to a functional component with React hooks for styles

--- Source/Plugins/Core/com.equella.core/js/tsrc/components/Error.tsx
+++ Source/Plugins/Core/com.equella.core/js/tsrc/components/Error.tsx
@@ -1,31 +1,29 @@
 import { Typography } from "@material-ui/core";
 import Paper from "@material-ui/core/Paper";
-import { Theme, WithStyles, withStyles } from "@material-ui/core/styles";
+import { Theme, makeStyles } from "@material-ui/core/styles";
 import * as React from "react";
+import { ReactNode, FunctionComponent } from "react";
 
-const styles = (theme: Theme) => {
-  return {
-    error: {
-      padding: theme.spacing(3),
-      backgroundColor: "rgb(255, 220, 220)",
-    },
-  };
-};
-
-interface ErrorProps {}
-
-type Props = ErrorProps & WithStyles<"error">;
-
-class Error extends React.Component<Props> {
-  render() {
-    return (
-      <Paper className={this.props.classes.error}>
-        <Typography color="error" align="center">
-          {this.props.children}
-        </Typography>
-      </Paper>
-    );
-  }
+interface ErrorProps {
+  children?: ReactNode;
 }
 
-export default withStyles(styles)(Error);
+const useStyles = makeStyles((theme: Theme) => ({
+  error: {
+    padding: theme.spacing(3),
+    backgroundColor: "rgb(255, 220, 220)",
+  },
+}));
+
+const Error: FunctionComponent<ErrorProps> = ({children}: ErrorProps) => {
+  const classes = useStyles();
+  return(
+    <Paper className={ classes.error } >
+      <Typography color="error" align="center">
+        {children}
+      </Typography>
+    </Paper>
+  );
+}
+
+export default Error;
