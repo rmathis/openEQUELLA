0e5d4fe63e58d4d02c5193eef5d2726d7e2d38ec
Ryan Mathis <rmathis@unicon.net>
2020-05-04 09:02:16 -0700

    Refactored MessageInfo component to a functional component with React hooks for styles

--- Source/Plugins/Core/com.equella.core/js/tsrc/components/MessageInfo.tsx
+++ Source/Plugins/Core/com.equella.core/js/tsrc/components/MessageInfo.tsx
@@ -6,13 +6,14 @@ import {
 } from "@material-ui/core";
 import amber from "@material-ui/core/colors/amber";
 import green from "@material-ui/core/colors/green";
-import { WithStyles, createStyles, withStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import CloseIcon from "@material-ui/icons/Close";
 import ErrorIcon from "@material-ui/icons/Error";
 import InfoIcon from "@material-ui/icons/Info";
 import WarningIcon from "@material-ui/icons/Warning";
 import * as React from "react";
+import { FunctionComponent } from "react";
 import { commonString } from "../util/commonstrings";
 
 const variantIcon = {
@@ -22,32 +23,31 @@ const variantIcon = {
   info: InfoIcon,
 };
 
-const styles = (theme: Theme) =>
-  createStyles({
-    success: {
-      backgroundColor: green[600],
-    },
-    error: {
-      backgroundColor: theme.palette.error.dark,
-    },
-    info: {
-      backgroundColor: theme.palette.primary.dark,
-    },
-    warning: {
-      backgroundColor: amber[700],
-    },
-    icon: {
-      fontSize: 20,
-    },
-    iconVariant: {
-      opacity: 0.9,
-      marginRight: theme.spacing(1),
-    },
-    message: {
-      display: "flex",
-      alignItems: "center",
-    },
-  });
+const useStyles = makeStyles((theme: Theme) => ({
+  success: {
+    backgroundColor: green[600],
+  },
+  error: {
+    backgroundColor: theme.palette.error.dark,
+  },
+  info: {
+    backgroundColor: theme.palette.primary.dark,
+  },
+  warning: {
+    backgroundColor: amber[700],
+  },
+  icon: {
+    fontSize: 20,
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing(1),
+  },
+  message: {
+    display: "flex",
+    alignItems: "center",
+  },
+}));
 
 interface MessageInfoProps {
   open: boolean;
@@ -56,37 +56,33 @@ interface MessageInfoProps {
   variant: "success" | "warning" | "error" | "info";
 }
 
-export default withStyles(styles)(
-  class MessageInfo extends React.Component<
-    MessageInfoProps & WithStyles<typeof styles>
-  > {
-    render() {
-      const { open, title, onClose, classes, variant } = this.props;
-      const Icon = variantIcon[variant];
-      return (
-        <Snackbar open={open} onClose={onClose} autoHideDuration={5000}>
-          <SnackbarContent
-            className={classes[variant]}
-            aria-describedby="client-snackbar"
-            message={
-              <span id="client-snackbar" className={classes.message}>
-                <Icon className={`${classes.icon} ${classes.iconVariant}`} />
-                {title}
-              </span>
-            }
-            action={
-              <IconButton
-                key="close"
-                aria-label={commonString.action.close}
-                color="inherit"
-                onClick={onClose}
-              >
-                <CloseIcon className={classes.icon} />
-              </IconButton>
-            }
-          />
-        </Snackbar>
-      );
-    }
-  }
-);
+const MessageInfo: FunctionComponent<MessageInfoProps> = ({ open, title, onClose, variant }: MessageInfoProps) => {
+  const classes = useStyles();
+  const Icon = variantIcon[variant];
+  return (
+    <Snackbar open={open} onClose={onClose} autoHideDuration={5000}>
+      <SnackbarContent
+        className={classes[variant]}
+        aria-describedby="client-snackbar"
+        message={
+          <span id="client-snackbar" className={classes.message}>
+            <Icon className={`${classes.icon} ${classes.iconVariant}`} />
+            {title}
+          </span>
+        }
+        action={
+          <IconButton
+            key="close"
+            aria-label={commonString.action.close}
+            color="inherit"
+            onClick={onClose}
+          >
+            <CloseIcon className={classes.icon} />
+          </IconButton>
+        }
+      />
+    </Snackbar>
+  );
+};
+
+export default MessageInfo;
