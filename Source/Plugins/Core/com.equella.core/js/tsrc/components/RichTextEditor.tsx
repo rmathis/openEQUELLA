e417cc8cd1a7e3c2e78cd7af62883722a9a58d45
Ryan Mathis <rmathis@unicon.net>
2020-05-04 09:05:24 -0700

    Refactored RichTextEditor component to a functional component with React hooks for styles

--- Source/Plugins/Core/com.equella.core/js/tsrc/components/RichTextEditor.tsx
+++ Source/Plugins/Core/com.equella.core/js/tsrc/components/RichTextEditor.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { FunctionComponent, useState, useEffect } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import { AxiosPromise, AxiosResponse } from "axios";
 import { baseURL } from "tinymce";
@@ -60,34 +61,25 @@ interface ImageReturnType {
   link: string;
 }
 
-interface RichTextEditorState {
-  ready: boolean;
-}
+const RichTextEditor: FunctionComponent<RichTextEditorProps> = (props: RichTextEditorProps) => {
+  const [ready, setReady] = useState(false);
 
-class RichTextEditor extends React.Component<
-  RichTextEditorProps,
-  RichTextEditorState
-> {
-  constructor(props: RichTextEditorProps) {
-    super(props);
-    this.state = { ready: false };
-  }
+  const { imageUploadCallBack, onStateChange, htmlInput } = props;
 
-  componentDidMount = () => {
+  useEffect(() => {
     setTimeout(() => {
       // this is a workaround for something related to: https://github.com/tinymce/tinymce-angular/issues/76
-      this.setState({ ready: true });
+      setReady(true);
     }, 1);
-  };
+  }, []);
 
-  uploadImages = (
+  const uploadImages = (
     blobInfo: BlobInfo,
     success: (msg: string) => void,
     failure: (msg: string) => void
   ): void => {
-    if (this.props.imageUploadCallBack) {
-      this.props
-        .imageUploadCallBack(blobInfo)
+    if (imageUploadCallBack) {
+      imageUploadCallBack(blobInfo)
         .then((response: AxiosResponse<ImageReturnType>) =>
           success(response.data.link)
         )
@@ -97,38 +89,38 @@ class RichTextEditor extends React.Component<
     }
   };
 
-  render() {
-    return (
-      this.state.ready && (
-        <Editor
-          init={{
-            min_height: 500,
-            automatic_uploads: true,
-            file_picker_types: "image",
-            images_reuse_filename: true,
-            images_upload_handler: this.uploadImages,
-            paste_data_images: true,
-            relative_urls: false,
-            skin: "oxide",
-            skin_url: `${baseURL}/tinymce/skins/ui/oxide`,
-            media_dimensions: false,
-          }}
-          toolbar={
-            "formatselect | bold italic strikethrough underline forecolor backcolor | link image media file | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent hr | removeformat | undo redo | preview | ltr rtl"
-          }
-          plugins={
-            "anchor autolink autoresize advlist charmap code codesample  " +
-            "directionality fullscreen help hr image imagetools " +
-            "importcss insertdatetime link lists media nonbreaking noneditable pagebreak paste " +
-            "preview print quickbars save searchreplace table template " +
-            "textpattern toc visualblocks visualchars wordcount"
-          }
-          onEditorChange={this.props.onStateChange}
-          value={this.props.htmlInput}
-        />
-      )
-    );
-  }
+  return (
+    <React.Fragment>
+    { ready && (
+      <Editor
+        init={{
+          min_height: 500,
+          automatic_uploads: true,
+          file_picker_types: "image",
+          images_reuse_filename: true,
+          images_upload_handler: uploadImages,
+          paste_data_images: true,
+          relative_urls: false,
+          skin: "oxide",
+          skin_url: `${baseURL}/tinymce/skins/ui/oxide`,
+          media_dimensions: false,
+        }}
+        toolbar={
+          "formatselect | bold italic strikethrough underline forecolor backcolor | link image media file | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent hr | removeformat | undo redo | preview | ltr rtl"
+        }
+        plugins={
+          "anchor autolink autoresize advlist charmap code codesample  " +
+          "directionality fullscreen help hr image imagetools " +
+          "importcss insertdatetime link lists media nonbreaking noneditable pagebreak paste " +
+          "preview print quickbars save searchreplace table template " +
+          "textpattern toc visualblocks visualchars wordcount"
+        }
+        onEditorChange={onStateChange}
+        value={htmlInput}
+      />
+    ) }
+    </React.Fragment>
+  );
 }
 
 export default RichTextEditor;
